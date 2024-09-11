60fa833cc1044ff571fadf66b96104ac956aae92
kassio03 <email@address.comkassioferreira0324@gmail.com>
2024-09-11 15:29:44 -0300

    IntersectionObserver implemented; adjustments on responsive design;

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,5 +1,6 @@
-import { useCallback, useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
 
 import Autumn from '../../assets/svg/autumn.svg?react';
 import Close from '../../assets/svg/close.svg?react';
@@ -15,15 +16,32 @@ import Winter from '../../assets/svg/winter.svg?react';
 import { handleChangeSeasonTheme, SeasonTheme } from '../../theme/handleChangeSeasonTheme';
 import { handleToggleMainTheme } from '../../theme/handleToggleMainTheme';
 import Icon from '../Icon';
-
 interface AsideProps {
   asideMustAppear: boolean;
   toggleAside: () => void;
 }
 
+const allIdsToObserver = {
+  onHome: [
+    { id: 'summary', label: 'Resumo' },
+    { id: 'experience', label: 'Experiência' },
+  ],
+  onFunctionalities: [
+    { id: 'animation', label: 'Animações' },
+    { id: 'language', label: 'Idiomas' },
+    { id: 'form', label: 'Formulários' },
+    { id: 'database', label: 'Banco de Dados' },
+    { id: 'graphics', label: 'Gráficos' },
+    { id: 'other', label: 'Outro' },
+    { id: 'abc', label: 'Outro' },
+  ],
+};
+
 const Aside = ({ asideMustAppear, toggleAside }: AsideProps) => {
   const [themeIcon, setThemeIcon] = useState('dark');
   const [highlightedSeason, setHighlightedSeason] = useState('');
+  const [currentItemOnScreen, setCurrentItemOnScreen] = useState('');
+
   const currentDate = useRef(
     new Date(Date.now()).toLocaleDateString('pt-BR', {
       day: 'numeric',
@@ -41,6 +59,41 @@ const Aside = ({ asideMustAppear, toggleAside }: AsideProps) => {
     setHighlightedSeason(season);
   }, []);
 
+  const location = useLocation().pathname;
+
+  useEffect(() => {
+    const elements = allIdsToObserver.onHome
+      .concat(allIdsToObserver.onFunctionalities)
+      .reduce((acum: HTMLElement[], item) => {
+        const element = document.getElementById(item.id);
+        if (element) acum.push(element);
+        return acum;
+      }, []);
+
+    const visibleItems: any = {};
+    const observer = new IntersectionObserver(items => {
+      items.forEach(item => {
+        visibleItems[item.target.id] = item.isIntersecting;
+      });
+      for (const key in visibleItems) {
+        if (visibleItems[key]) {
+          setCurrentItemOnScreen(key);
+          break;
+        }
+      }
+    });
+
+    elements.forEach(item => {
+      observer.observe(item);
+    });
+
+    return () => {
+      elements.forEach(item => {
+        observer.unobserve(item);
+      });
+    };
+  });
+
   return (
     <div
       className={`fixed top-0 z-[9999] h-screen w-full flex-col items-center bg-solidPrimary/95 md:bg-solidPrimary/90 lg:sticky lg:max-w-[360px] ${asideMustAppear ? 'hidden lg:flex' : 'flex'}`}
@@ -54,7 +107,7 @@ const Aside = ({ asideMustAppear, toggleAside }: AsideProps) => {
           onClick={toggleTheme}
         />
       </div>
-      <div className="h-[223px] w-[220px]">
+      <div className="h-[223px] w-[220px] [@media(max-height:480px)]:hidden">
         <div className="mx-auto flex h-[74px] w-[180px] justify-between">
           <Lines className="rotate-0 self-end" />
           <Icon
@@ -94,16 +147,50 @@ const Aside = ({ asideMustAppear, toggleAside }: AsideProps) => {
           <Lines className="rotate-180" />
         </div>
       </div>
-      <div>
-        <Link to={'/home'} className="mt-11 flex items-center">
+      <nav className="overflow-y-auto">
+        <Link to={'/home'} className="mx-4 mt-11 flex items-center">
           <Icon Svg={Person} className="mx-2 h-6 w-6" />
           <span className="text-lg">RESUMO PROFISSIONAL</span>
         </Link>
-        <Link to={'/functionalities'} className="mt-8 flex items-center">
+        <ul
+          className={`${location !== '/functionalities' ? 'block' : 'hidden'} ml-14 flex flex-col`}
+        >
+          {allIdsToObserver.onHome.map(item => {
+            return (
+              <li key={item.id}>
+                <HashLink
+                  className={`${currentItemOnScreen === item.id ? 'text-solidSeason' : null}`}
+                  to={`#${item.id}`}
+                  smooth
+                >
+                  {item.label}
+                </HashLink>
+              </li>
+            );
+          })}
+        </ul>
+        <Link to={'/functionalities'} className="mx-4 mt-8 flex items-center">
           <Icon Svg={Resources} className="m-2 h-6 w-6" />
           <span className="text-lg">RECURSOS PARA TESTAR</span>
         </Link>
-      </div>
+        <ul
+          className={`${location === '/functionalities' ? 'block' : 'hidden'} ml-14 flex flex-col`}
+        >
+          {allIdsToObserver.onFunctionalities.map(item => {
+            return (
+              <li key={item.id}>
+                <HashLink
+                  className={`${currentItemOnScreen === item.id ? 'text-solidSeason' : null}`}
+                  to={`#${item.id}`}
+                  smooth
+                >
+                  {item.label}
+                </HashLink>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
       <p className="mb-[20px] mt-auto w-[185px] text-center text-sm">
         São Paulo
         <br /> {currentDate.current}
