7ae16f500ea5988cb9ddd65db01e6e6cf33c5259
kassio03 <email@address.comkassioferreira0324@gmail.com>
2024-09-22 22:36:23 -0300

    handleToggleMainTheme name changed; ThemeSection responsiveness fixes;

--- src/components/ThemeSection/index.tsx
+++ src/components/ThemeSection/index.tsx
@@ -10,33 +10,40 @@ const ThemeSection = () => {
   const { setMainTheme, setSeasonTheme } = useTheme();
   return (
     <section className="relative w-full rounded-[28px] bg-solidPrimary/90 p-8 lg:px-12">
-      <header className="relative z-10 flex flex-wrap items-center justify-center text-sm min-[425px]:justify-between">
-        <div className="mb-3 flex items-center min-[425px]:mb-0">
-          <Icon className="h-7 w-7 !cursor-default stroke-0" Svg={ThemeIcon} />
+      <header className="relative z-10 flex flex-wrap items-center justify-center gap-3 text-sm min-[425px]:justify-between">
+        <div className="flex flex-grow-[10] items-center">
+          <Icon className="f h-7 w-7 !cursor-default stroke-0" Svg={ThemeIcon} />
           <h2 className="ml-3 text-4xl">Tematização</h2>
         </div>
-        <Select
-          id="a"
-          width="160px"
-          placeholder="Tema principal"
-          options={['Claro', 'Escuro']}
-          values={[Theme.light, Theme.dark]}
-          handleClick={(value: Theme) => {
-            setMainTheme(value);
-          }}
-        />
-        <Select
-          id="s"
-          width="160px"
-          placeholder="Tema de estação"
-          options={['Primavera', 'Verão', 'Outono', 'Inverno']}
-          values={[SeasonTheme.spring, SeasonTheme.summer, SeasonTheme.autumn, SeasonTheme.winter]}
-          handleClick={(value: SeasonTheme) => {
-            setSeasonTheme(value);
-          }}
-        />
+        <div className="bg-red flex flex-grow flex-wrap justify-center gap-6">
+          <Select
+            id="mainThemes"
+            width="160px"
+            placeholder="Tema principal"
+            options={['Claro', 'Escuro']}
+            values={[Theme.light, Theme.dark]}
+            handleClick={(value: Theme) => {
+              setMainTheme(value);
+            }}
+          />
+          <Select
+            id="seasonThemes"
+            width="160px"
+            placeholder="Tema de estação"
+            options={['Primavera', 'Verão', 'Outono', 'Inverno']}
+            values={[
+              SeasonTheme.spring,
+              SeasonTheme.summer,
+              SeasonTheme.autumn,
+              SeasonTheme.winter,
+            ]}
+            handleClick={(value: SeasonTheme) => {
+              setSeasonTheme(value);
+            }}
+          />
+        </div>
       </header>
-      <p className="my-6">
+      <p className="my-6 text-justify">
         Você também pode alterar o tema rapidamente clicando nos icones no menu a esquerda.
       </p>
 
