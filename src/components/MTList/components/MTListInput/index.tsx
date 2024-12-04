8c4cdaf81b222108f7142a0ce8ced110250e2870
kassio03 <email@address.comkassioferreira0324@gmail.com>
2024-12-04 12:40:32 -0300

    MTListInput Svg is now optional

--- src/components/MTList/components/MTListInput/index.tsx
+++ src/components/MTList/components/MTListInput/index.tsx
@@ -1,7 +1,7 @@
 import { InputHTMLAttributes } from 'react';
 
 interface MTListInputProps extends InputHTMLAttributes<HTMLInputElement> {
-  Svg: React.FunctionComponent<
+  Svg?: React.FunctionComponent<
     React.SVGProps<SVGSVGElement> & {
       title?: string;
     }
@@ -18,7 +18,7 @@ const MTListInput = ({
 }: MTListInputProps) => {
   return (
     <div className={`relative flex items-center ${customClassContainer}`}>
-      <Svg className="pointer-events-none absolute left-4" />
+      {Svg && <Svg className="pointer-events-none absolute left-4" />}
       <input
         className={`h-10 w-full rounded-[10px] border-2 border-[#FFB573] bg-transparent indent-12 placeholder-[#FFB573]/50 ${customClassInput}`}
         {...rest}
